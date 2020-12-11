7316feac8f3acf94b065c9e44ab0d653a8a56cec
Igor Srdoc <igor.srdoc@visionect.com>
2020-12-11 18:00:36 +0100

    Add MaterialUI

---
+++ src/styles/theme.jsx
@@ -0,0 +1,11 @@
+import { createMuiTheme, Theme } from '@material-ui/core'
+
+const theme: Theme = createMuiTheme({
+  palette: {
+    // primary: {
+    //   main: '#ffffff',
+    // },
+  },
+})
+
+export default theme
